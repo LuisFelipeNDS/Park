880ff3437bc8da31472b74fc36476677c28fe32a
Felipe <Felipesss.2014@gmail.com>
2024-01-29 19:00:55 -0300

    Testes adicionados

--- src/test/java/com/lfn/UsuarioIT.java
+++ src/test/java/com/lfn/UsuarioIT.java
@@ -1,13 +1,39 @@
 package com.lfn;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.lfn.Web.Dto.UsuarioCreateDto;
+import com.lfn.Web.Dto.UsuarioResponseDto;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/resources/Sql/Usuario/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/resources/Sql/Usuario/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UsuarioIT {
 	
 	@Autowired
 	WebTestClient testClient;
 
+	@Test
+	public void createUser_ComUsernameEPasswordValidos_RetornarUsuarioCriadoComStatus201() {
+		
+		UsuarioResponseDto  responseBody = testClient
+				.post()
+				.uri("api/v1/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(new UsuarioCreateDto("Tody@email.com", "123456"))
+				.exchange().expectStatus().isCreated()
+				.expectBody(UsuarioResponseDto.class)
+				.returnResult().getResponseBody();
+				
+		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@email.com");
+		org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+	}	
 }
